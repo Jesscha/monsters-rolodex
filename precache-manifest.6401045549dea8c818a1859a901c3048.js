855fa78971617bb015ace5c888f5fd7fe17063d5
jessecha <jessecha222@gmail.com>
2020-04-09 23:02:55 +0900

    Updates

---
+++ precache-manifest.6401045549dea8c818a1859a901c3048.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f46cb6f8a47ce32bdb731691f40a0b90",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d5ca3db6fd20f8ae5c6f",
+    "url": "/monsters-rolodex/static/css/main.aceef826.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ca3db6fd20f8ae5c6f",
+    "url": "/monsters-rolodex/static/js/main.fb6f2395.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
